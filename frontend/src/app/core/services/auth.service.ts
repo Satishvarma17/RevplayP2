5498ee8d03ca81db9ddb6fcf84910191b295ad26
Srinivas-han <madhuhanumanthu123@gmail.com>
2026-02-26 16:11:25 +0530

    solved errors in devlop

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:8082/api/auth';
+  private baseUrl = 'http://localhost:8083/api/auth';
 
   constructor(private http: HttpClient) {}
 
